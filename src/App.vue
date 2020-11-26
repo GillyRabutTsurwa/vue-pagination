a6054867cedc2d22293ce3a3d2e75c774c05b214
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-11-25 20:08:01 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import HelloWorld from "./components/HelloWorld.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      posts: [],
+      loading: false,
+      //pagination variables
+      currentPage: 1,
+      postsPerPage: 10,
+    };
+  },
+  methods: {
+    async fetchPosts() {
+      this.loading = true;
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = response.data;
+      console.log(data);
+      this.loading = false;
+    },
+  },
+  async created() {
+    this.fetchPosts();
+  },
+};
 </script>
 
 <style>

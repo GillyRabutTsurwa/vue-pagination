5d0aebf983eb1d569ac78c3940a18d7a81b02ddd
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-11-27 19:15:26 -0600

    made a separate Post component of which the PostList component will consist

---
+++ src/components/Post.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    {{currentPostProp.title}}
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    currentPostProp: {
+      type: Object,
+      required: true,
+    },
+  },
+  created() {
+    console.log(this.currentPostProp);
+  },
+};
+</script>
+
+<style>
+</style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,15 +1,20 @@
 <template>
   <h2 v-if="loadingProp">Loading.... / Chargement....</h2>
   <ul>
-    <li v-for="currentPost in postsProp" v-bind:key="currentPost.id">
+    <!-- <li v-for="currentPost in postsProp" v-bind:key="currentPost.id">
       {{currentPost.title}}
-    </li>
+    </li> -->
+    <CurrentPost v-for="currentPost in postsProp" v-bind:key="currentPost.id" v-bind:currentPostProp="currentPost" />
   </ul>
 </template>
 
 <script>
+import Post from "./Post";
 export default {
   name: "PostList",
+  components: {
+    CurrentPost: Post,
+  },
   props: {
     postsProp: {
       type: Array,
